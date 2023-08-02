4311ce037b0dc385ef49062734456310d7ae26d7
cruisershiva <123819299+cruisershiva@users.noreply.github.com>
2023-08-02 14:02:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+
+var npark = 4;
+var timeData = {};
+var url = 'localhost:3';
+const cors = require('cors')
+app.use(cors());
+var loadURL = function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDr_W2TH9vNJG0XKANkfEkI0MZyxExMsug",
+        authDomain: "iot-ssh.firebaseapp.com",
+        databaseURL: "https://iot-ssh-default-rtdb.firebaseio.com",
+        projectId: "iot-ssh",
+        storageBucket: "iot-ssh.appspot.com",
+        messagingSenderId: "249538182307",
+        appId: "1:249538182307:web:033c3123275da03128d316",
+        measurementId: "G-KGBCC432RD"
+    };
+    const app = initializeApp(firebaseConfig);
+
+    const db = getDatabase(app);
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `link/ssh`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            url = snapshot.val();
+            var socket = io(url);
+            socket.on('sensor', function(data) {
+                parking(data);
+                checkGate(data);
+                // console.log(data);
+            });
+            console.log(url);
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+var getElapsedTime = function(start) {
+    let timeDiff = new Date().getTime() - start.getTime();
+    timeDiff = timeDiff / 1000;
+    let seconds = Math.round(timeDiff % 60);
+    timeDiff = Math.floor(timeDiff / 60);
+    let minutes = timeDiff % 60;
+    timeDiff = Math.floor(timeDiff / 60);
+    let hours = timeDiff % 24;
+    timeDiff = Math.floor(timeDiff / 24);
+    hours = hours + timeDiff * 24;
+    if (hours == 0) {
+        if (minutes == 0)
+            return seconds + "sec"
+        return minutes + "min " + seconds + "sec";
+    }
+    return hours + "hr " + minutes + "min " + seconds + "sec";
+};
+
+var getTimer = function(start, elem) {
+    return function() {
+        elem.text(getElapsedTime(start));
+    };
+};
+
+var checkGate = function(data) {
+    try {
+        var gateData = data.gate;
+    } catch (e) {}
+
+    if (gateData == 1) {
+        $("#gate").text("OPEN");
+        $("#gate").parent().removeClass("closed");
+        $("#gate").parent().addClass("open");
+    } else if (gateData == 0) {
+        $("#gate").text("CLOSED");
+        $("#gate").parent().removeClass("open");
+        $("#gate").parent().addClass("closed");
+    }
+};
+
+var parking = function(data) {
+    var filled = 0;
+    for (let i = 1; i <= npark; i++) {
+        let irsensor = `ir${i}`
+
+        try {
+            var ir = data[irsensor];
+        } catch (e) {}
+
+        let park = $(`#park${i}`);
+        let parkState = $(`#parkState${i}`);
+        let entryTime = $(`#entryTime${i}`);
+        let elapTime = $(`#elapTime${i}`);
+        let timedata = timeData[irsensor];
+
+        if (ir == 1 && timedata.entryTime == 0) {
+            parkState.text("Occupied");
+            park.removeClass("vacant");
+            park.addClass("occupied");
+
+            timedata.entryTime = new Date();
+            entryTime.text(timedata.entryTime.toLocaleTimeString());
+            timedata.elapTime = setInterval(
+                getTimer(timedata.entryTime, elapTime),
+                1000
+            );
+        } else if (ir == 0 && timedata.entryTime != 0) {
+            parkState.text("Vacant");
+            park.addClass("vacant");
+            park.removeClass("occupied");
+
+            clearInterval(timedata.elapTime);
+            timedata.elapTime = 0;
+            timedata.entryTime = 0;
+            entryTime.text("");
+            elapTime.text("");
+        }
+        filled += parseInt(ir);
+    }
+    $("#slot").text(npark - filled);
+};
+
+window.onload = function() {
+    for (let i = 1; i <= npark; i++) {
+        timeData[`ir${i}`] = {
+            "entryTime": 0,
+            "elapTime": 0
+        };
+    }
+    loadURL();
+};
---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const socket = require('socket.io');
+const path = require('path');
+const db = require('./database.js')
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/webpage')));
+
+
+
+// const express = require("express")
+// var app = express();
+// var server = app.listen(4000);
+// var io = require('socket.io')(server, {
+//     cors: {
+//       origin: '*',
+//     }
+// });
+
+
+
+
+
+
+const portn = process.env.PORT;
+const server = app.listen(portn, () => {
+    console.log(`server started on http://localhost:${portn}/`);
+    db.database();
+});
+/*
+ * This portion of code begins a serial connection between the arduino board
+ * which is assumed to be connected to COM3 port number at 9600 baud rate.
+ */
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const port = new SerialPort({ path: "COM3", baudRate: 9600 });
+const parser = new ReadlineParser();
+port.pipe(parser);
+
+var data;
+
+/*
+ * Starting a worker thread which will simulate a sensor and send the data 
+ * as a message event to the server. Usage: In case the board is not connected.
+ * If board's sensor data is required, comment out the following lines.
+ */
+const { Worker } = require('worker_threads');
+var sensor = new Worker('./sensor.js');
+sensor.on('message', function(val) {
+    data = val;
+    console.log(val);
+});
+
+
+parser.on('data', function(info) {
+    data = JSON.parse(info);
+    console.log(data);
+});
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+        handlePreflightRequests: (req, res) => {
+            res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET",
+                "Access-Control-Allow-Headers": "my-custom-header",
+                "Access-Control-Allow-Credentials": "true"
+            });
+            res.end();
+        }
+    }
+});
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    setInterval(function() {
+        socket.emit('sensor', data);
+    }, 10);
+    socket.on('disconnect', function() {
+        console.log('A user disconnected');
+    });
+});
+
+app.get("/ssh", (req, res) => {
+    console.log(process.env.SSH);
+    res.send(process.env.SSH);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
